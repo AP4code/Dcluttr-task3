cfc21565f2fc0507d1de035f4b0b6b16732ba4ed
Aditya <55946826+AP4code@users.noreply.github.com>
2025-07-22 15:14:35 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Layout from '@/components/Layout'
+
+export default function Home() {
+  return (
+    <Layout>
+      <h1 className="text-xl font-semibold">Quick Commerce</h1>
+    </Layout>
+  )
+}
